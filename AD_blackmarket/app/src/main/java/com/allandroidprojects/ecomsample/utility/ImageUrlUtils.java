09d137f7cc88d5c7cf49ad0b11f2e58ac6194b42
Aprizal Kamran <rizalrheynza@github.com>
2017-12-04 03:51:48 +0800

    Project TOST
    Project aprizal & deo

---
+++ AD_blackmarket/app/src/main/java/com/allandroidprojects/ecomsample/utility/ImageUrlUtils.java
@@ -0,0 +1,153 @@
+package com.allandroidprojects.ecomsample.utility;
+
+import java.util.ArrayList;
+
+/**
+ * Created by 06peng on 2015/6/24.
+ */
+public class ImageUrlUtils {
+    static ArrayList<String> wishlistImageUri = new ArrayList<>();
+    static ArrayList<String> cartListImageUri = new ArrayList<>();
+
+    public static String[] getImageUrls() {
+        String[] urls = new String[] {
+                "https://assets.academy.com/mgen/03/20041003.jpg",
+                "http://www.dunhamssports.com/wp-content/uploads/2017/04/SW-MP45-Shield.jpg",
+                "https://assets.academy.com/mgen/75/10330375.jpg",
+                "https://assets.academy.com/mgen/71/10817571.jpg",
+                "https://assets.academy.com/mgen/36/10496636.jpg",
+                "https://assets.academy.com/mgen/23/10681023.jpg",
+                "https://www.gilbertsguns.com/media/catalog/product/cache/1/image/600x600/9df78eab33525d08d6e5fb8d27136e95/s/p/springfield_px9116lp.jpg",
+                "http://sts-cr.com/wp-content/uploads/2017/03/Walther-P22Q-1-600x600.jpg",
+                "https://onestopfirearms.com/wp-content/uploads/2017/07/assetsimageslcp2-600x600.jpg",
+                "https://cdn.shopify.com/s/files/1/0276/1899/products/Walther-MPL-K-SMG.jpg?v=1387131841",
+                "https://cdn.shopify.com/s/files/1/0276/1899/products/NBFV_IMI_Uzi.jpg?v=1390282544",
+                "https://cdn.shopify.com/s/files/1/0276/1899/products/FN-P90-SMG.jpg?v=1387130673",
+                "https://cdn.shopify.com/s/files/1/0276/1899/products/KRISS-Super-V-SMG.jpg?v=1387094062",
+                "https://cdn.shopify.com/s/files/1/0276/1899/products/Colt_9mm_SMG_600px.jpg?v=1387251386",
+                "https://cdn.shopify.com/s/files/1/0276/1899/products/Steyr-MPi-81-SMG.jpg?v=1387131744",
+                "https://cdn.shopify.com/s/files/1/0276/1899/products/HK-UMP-45-SMG.jpg?v=1387131200",
+                "https://cdn.shopify.com/s/files/1/0276/1899/products/HK-MP5-A3-SMG.jpg?v=1387130800",
+                "http://www.airstrike.lt/image/cache/data/product-134/1152192065_1-600x600.jpg",
+                "http://allstatepolice.com/image/cache/data/colt/R0991-600x600.jpg",
+                "http://www.airstrike.lt/image/cache/data/product-3322/FB3004(1)-600x600.jpg",
+                "http://www.airstrike.lt/image/cache/data/product-6412/1152205423_1-600x600.jpg",
+                "http://www.airstrike.lt/image/cache/data/product-856/1152199203_1-600x600.jpg",
+                "http://www.airstrike.lt/image/cache/data/product-2489/1152205140_6-600x600.jpg",
+                "https://cdn.shopify.com/s/files/1/0276/1899/products/M4A1600px.jpg?v=1387144032",
+                "https://wallscover.com/images/tavor-assault-rifle-wallpaper-3.jpg",
+                "https://wallscover.com/images/vz-58-assault-rifle-wallpaper-10.jpg",
+                "https://cdn.shopify.com/s/files/1/0276/1899/products/AMD-65-Assault-Rifle.jpg?v=1387091812",
+                "https://cdn.shopify.com/s/files/1/0276/1899/products/Steyr-AUG-Assault-Rifle.jpg?v=1387133247",
+                "https://cdn.shopify.com/s/files/1/0276/1899/products/Galil-Assault-Rifle.jpg?v=1387092362",
+                "http://www.airstrike.lt/image/cache/data/product-432/1152196302_1-600x600.jpg",
+                "http://www.topgunclothing.co.uk/image/cache/data/products/Ares/AM-009-BK_L-600x600.jpg",
+                "http://www.airstrike.lt/image/cache/data/product-1107/1152200144_1-600x600.jpg",
+                "http://www.airstrike.lt/image/cache/data/product-398/1152195886_1-600x600.jpg",
+                "http://www.airstrike.lt/image/cache/data/product-20373/1152203608_1-600x600.jpg",
+
+
+        };
+        return urls;
+    }
+
+    public static String[] getPistolsUrls() {
+        String[] urls = new String[]{
+                "https://assets.academy.com/mgen/03/20041003.jpg",
+                "http://www.dunhamssports.com/wp-content/uploads/2017/04/SW-MP45-Shield.jpg",
+                "https://assets.academy.com/mgen/75/10330375.jpg",
+                "https://assets.academy.com/mgen/71/10817571.jpg",
+                "https://assets.academy.com/mgen/36/10496636.jpg",
+                "https://assets.academy.com/mgen/23/10681023.jpg",
+                "https://www.gilbertsguns.com/media/catalog/product/cache/1/image/600x600/9df78eab33525d08d6e5fb8d27136e95/s/p/springfield_px9116lp.jpg",
+                "http://sts-cr.com/wp-content/uploads/2017/03/Walther-P22Q-1-600x600.jpg",
+                "https://onestopfirearms.com/wp-content/uploads/2017/07/assetsimageslcp2-600x600.jpg"
+        };
+        return urls;
+    }
+
+    public static String[] getSmgUrls() {
+        String[] urls = new String[]{
+                "https://cdn.shopify.com/s/files/1/0276/1899/products/Walther-MPL-K-SMG.jpg?v=1387131841",
+                "https://cdn.shopify.com/s/files/1/0276/1899/products/NBFV_IMI_Uzi.jpg?v=1390282544",
+                "https://cdn.shopify.com/s/files/1/0276/1899/products/FN-P90-SMG.jpg?v=1387130673",
+                "https://cdn.shopify.com/s/files/1/0276/1899/products/KRISS-Super-V-SMG.jpg?v=1387094062",
+                "https://cdn.shopify.com/s/files/1/0276/1899/products/Colt_9mm_SMG_600px.jpg?v=1387251386",
+                "https://cdn.shopify.com/s/files/1/0276/1899/products/Steyr-MPi-81-SMG.jpg?v=1387131744",
+                "https://cdn.shopify.com/s/files/1/0276/1899/products/HK-UMP-45-SMG.jpg?v=1387131200",
+                "https://cdn.shopify.com/s/files/1/0276/1899/products/HK-MP5-A3-SMG.jpg?v=1387130800",
+                "http://www.airstrike.lt/image/cache/data/product-134/1152192065_1-600x600.jpg",
+                "http://allstatepolice.com/image/cache/data/colt/R0991-600x600.jpg",
+                "http://www.airstrike.lt/image/cache/data/product-3322/FB3004(1)-600x600.jpg",
+                "http://www.airstrike.lt/image/cache/data/product-6412/1152205423_1-600x600.jpg",
+                "http://www.airstrike.lt/image/cache/data/product-856/1152199203_1-600x600.jpg",
+                "http://www.airstrike.lt/image/cache/data/product-2489/1152205140_6-600x600.jpg"
+        };
+        return urls;
+    }
+
+    public static String[] getAssaultRilfesUrls() {
+        String[] urls = new String[]{
+                "https://cdn.shopify.com/s/files/1/0276/1899/products/M4A1600px.jpg?v=1387144032",
+                "https://wallscover.com/images/tavor-assault-rifle-wallpaper-3.jpg",
+                "https://wallscover.com/images/vz-58-assault-rifle-wallpaper-10.jpg",
+                "https://cdn.shopify.com/s/files/1/0276/1899/products/AMD-65-Assault-Rifle.jpg?v=1387091812",
+                "https://cdn.shopify.com/s/files/1/0276/1899/products/Steyr-AUG-Assault-Rifle.jpg?v=1387133247",
+                "https://cdn.shopify.com/s/files/1/0276/1899/products/Galil-Assault-Rifle.jpg?v=1387092362",
+                "http://www.airstrike.lt/image/cache/data/product-432/1152196302_1-600x600.jpg",
+                "http://www.topgunclothing.co.uk/image/cache/data/products/Ares/AM-009-BK_L-600x600.jpg",
+                "http://www.airstrike.lt/image/cache/data/product-1107/1152200144_1-600x600.jpg",
+                "http://www.airstrike.lt/image/cache/data/product-398/1152195886_1-600x600.jpg",
+                "http://www.airstrike.lt/image/cache/data/product-20373/1152203608_1-600x600.jpg"
+        };
+        return urls;
+    }
+
+    public static String[] getSniperRilfesUrls() {
+        String[] urls = new String[]{
+                "http://www.airstrike.lt/image/cache/data/product-1814/1152203078_1-600x600.jpg",
+                "http://atlas-content-cdn.pixelsquid.com/stock-images/sniper-rifle-vss-vintorez-n1VJVoA-600.jpg",
+                "http://www.airstrike.lt/image/cache/data/product-19344/1152196220_1-600x600.jpg",
+                "http://www.airstrike.lt/image/cache/data/product-5361/1152192737_13-600x600.jpg",
+                "http://www.airstrike.lt/image/cache/data/product-1966/1152203493_6-600x600.jpg",
+                "http://www.airstrike.lt/image/cache/data/product-6518/1152205920_1-600x600.jpg",
+                "http://www.airstrike.lt/image/cache/data/product-1814/1152203078_1-600x600.jpg",
+                "http://www.airstrike.lt/image/cache/data/product-1814/1152203078_1-600x600.jpg",
+                "http://www.airstrike.lt/image/cache/data/product-599/1152197816_1-600x600.jpg"
+        };
+        return urls;
+    }
+
+    public static String[] getVehiclesUrls() {
+        String[] urls = new String[]{
+                "https://i.pinimg.com/736x/80/d2/3d/80d23dc068eadb30430082c0530156ff--military-vehicles-d-models.jpg",
+                "https://360view.hum3d.com/zoom/Tanks/ASCOD_Ulan_1000x1000_0001.jpg",
+                "https://360view.hum3d.com/zoom/Tanks/ZBL_09_1000x1000_0001.jpg",
+                "https://360view.hum3d.com/zoom/Tanks/Freccia_IFV_1000_0001.jpg",
+                "https://360view.hum3d.com/zoom/Tanks/Ratel_IFV_1000x1000_0001.jpg"
+        };
+        return urls;
+    }
+
+    // Methods for Wishlist
+    public void addWishlistImageUri(String wishlistImageUri) {
+        this.wishlistImageUri.add(0,wishlistImageUri);
+    }
+
+    public void removeWishlistImageUri(int position) {
+        this.wishlistImageUri.remove(position);
+    }
+
+    public ArrayList<String> getWishlistImageUri(){ return this.wishlistImageUri; }
+
+    // Methods for Cart
+    public void addCartListImageUri(String wishlistImageUri) {
+        this.cartListImageUri.add(0,wishlistImageUri);
+    }
+
+    public void removeCartListImageUri(int position) {
+        this.cartListImageUri.remove(position);
+    }
+
+    public ArrayList<String> getCartListImageUri(){ return this.cartListImageUri; }
+}
